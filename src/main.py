b5393c977f4d03fb2331260df8ac1dfdb8e38f6b
alepiana <alessandropiana@icloud.com>
2026-02-01 18:25:22 +0100

    add main api

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+# Definizione del formato dei dati in ingresso
+class Transaction(BaseModel):
+    feature1: float
+    feature2: float
+    feature3: float
+    feature4: float
+
+app = FastAPI()
+
+# Carichiamo il modello (il cervello)
+MODEL_PATH = "models/fraud_model.joblib"
+model = joblib.load(MODEL_PATH)
+
+@app.get("/")
+def home():
+    return {"status": "Online", "model_version": "0.1.0"}
+
+@app.post("/predict")
+def predict_fraud(data: Transaction):
+    # Trasformiamo i dati ricevuti in un formato leggibile dal modello (array)
+    input_data = np.array([[data.feature1, data.feature2, data.feature3, data.feature4]])
+    
+    # Chiediamo al modello: "È una truffa?"
+    prediction = model.predict(input_data)
+    
+    # Restituiamo il risultato (convertendo da formato numpy a intero Python)
+    return {
+        "is_fraud": int(prediction[0]),
+        "probability": "Il modello ha deciso!"
+    }
